4cc91d1d94d2eb1d18e482f50b9d977596c99afd
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-10-21 03:26:18 +0800

    Implement Increase or Smash problem solution

---
+++ 2152/2152A-Increase_or_Smash.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int n, ans = 0; cin >> n;
+        vector<int> counts(101, 0);
+        for(int i = 0; i < n; i++){
+            int a; cin >> a;
+            if(!counts[a]) ans++, counts[a]++;
+        }
+        cout << ans-- + ans << "\n";
+    }
+}
