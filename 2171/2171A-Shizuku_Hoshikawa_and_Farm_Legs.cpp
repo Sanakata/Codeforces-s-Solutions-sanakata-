830336057ad30676f9d4db791269492abb5b7f21
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-12-01 10:13:55 +0800

    Create 2171A-Shizuku_Hoshikawa_and_Farm_Legs.cpp

---
+++ 2171/2171A-Shizuku_Hoshikawa_and_Farm_Legs.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        cout << (n % 2 == 1 ? 0 : n / 4 + 1) << "\n";
+    }
+}
