464fe10f7ccc6298fefcee432a5f4375ae904960
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-12-13 12:21:10 +0800

    Implement solution for Difficult Contest problem

---
+++ 2125/2125A-Difficult_Contest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        string s; cin >> s;
+        sort(s.begin(), s.end(), greater<int>());
+        cout << s << "\n";
+    }
+}
