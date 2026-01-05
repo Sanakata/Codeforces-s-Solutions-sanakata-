37a513f1db50abb3b4ea007cd8cfc2f120c814c1
Sanakata <155560381+Sanakata@users.noreply.github.com>
2026-01-05 20:45:22 +0800

    Add main function to process input and output sums

---
+++ 1772/1772A-A+B?.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        string s; cin >> s;
+        cout << (s[0] - '0') + (s[2] - '0') << "\n";
+    }
+}
