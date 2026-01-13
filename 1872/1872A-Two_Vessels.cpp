281ed423c771b7a44c91024edc7fbda751936d06
Sanakata <155560381+Sanakata@users.noreply.github.com>
2026-01-13 22:23:37 +0800

    Create 1872A-Two_Vessels.cpp

---
+++ 1872/1872A-Two_Vessels.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false); cin.tie(NULL);
+	int t; cin >> t;
+	while(t--){
+		int a, b, c; cin >> a >> b >> c;
+		cout << ceil((double) abs(a - b) / 2 / c) << "\n";
+	}
+}
