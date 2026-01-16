3454569fd508c5caed8d0867b0bd50da503778f8
Sanakata <155560381+Sanakata@users.noreply.github.com>
2026-01-16 20:53:58 +0800

    Add main function for Greatest Convex problem

---
+++ 1768/1768A-Greatest_Convex.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int k; cin >> k;
+        cout << k - 1 << "\n";
+    }
+}
