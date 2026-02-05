07409ab65675023a6b624a72f2d8e453fd834390
Sanakata <155560381+Sanakata@users.noreply.github.com>
2026-02-05 16:06:21 +0800

    Implement median finding in 984A-Game.cpp

---
+++ 984/984A-Game.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n; cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    cout << a[n / 2 - (n % 2 == 0)] << "\n";
+}
